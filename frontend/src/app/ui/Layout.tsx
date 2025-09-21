e1096cc8cb9710bc5430b840712e584d2aef0d9a
Evgeny Yanchanko <yauheni.yanchanka@gmail.com>
2025-09-21 14:46:04 +0300

    fixed Layout

--- frontend/src/app/ui/Layout.tsx
+++ frontend/src/app/ui/Layout.tsx
@@ -9,9 +9,8 @@ import { ROUTES } from "../routing";
 
 const layoutStyles = tv({
   slots: {
-    wrapper: "min-h-full flex flex-col",
     header: "h-[4rem] lg:h-[7rem]",
-    main: "flex-auto h-[calc(100vh-8rem)] lg:h-[calc(100vh-7rem)] mt-[4rem] lg:mt-[7rem] mb-[4rem] lg:mb-0",
+    main: "fixed top-[4rem] lg:top-[7rem] bottom-[4rem] lg:bottom-0 right-0 left-0 overflow-auto bg-secondary",
     themeToggle: "ml-auto",
     desktop: "hidden lg:flex",
     mobile: "fixed z-10 bottom-0 h-[4rem] lg:hidden",
@@ -19,16 +18,15 @@ const layoutStyles = tv({
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const { wrapper, header, main, themeToggle, desktop, mobile } =
-    layoutStyles();
+  const { header, main, themeToggle, desktop, mobile } = layoutStyles();
   return (
-    <div className={wrapper()}>
+    <>
       <Header className={header()}>
         <Navigation className={desktop()} routes={ROUTES} />
         <ThemeToggle className={themeToggle()} />
       </Header>
       <main className={main()}>{children}</main>
       <Navigation className={mobile()} routes={ROUTES} />
-    </div>
+    </>
   );
 }
