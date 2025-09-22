16b374106d827f8d8ff454e18da817c2c769df3e
Evgeny Yanchanko <yauheni.yanchanka@gmail.com>
2025-09-22 11:29:56 +0300

    added Converter page

--- frontend/src/pages/converter/Converter.tsx
+++ frontend/src/pages/converter/Converter.tsx
@@ -1,3 +1,19 @@
+import { tv } from "tailwind-variants";
+
+import { ConverterCard } from "@/widgets/converterCard";
+
+const converterStyles = tv({
+  slots: {
+    wrapper: "min-h-full h-max-content flex justify-center items-center",
+    converterCard: "w-screen max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl mx-3",
+  },
+});
+
 export default function Converter() {
-  return <></>;
+  const { wrapper, converterCard } = converterStyles();
+  return (
+    <article className={wrapper()}>
+      <ConverterCard className={converterCard()} />
+    </article>
+  );
 }
