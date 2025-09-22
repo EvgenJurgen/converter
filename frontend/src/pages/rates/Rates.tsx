6e30ab28cb6e6c97553edb9c9f442abe2fde40d5
Evgeny Yanchanko <yauheni.yanchanka@gmail.com>
2025-09-22 11:30:25 +0300

    added Rates page

--- frontend/src/pages/rates/Rates.tsx
+++ frontend/src/pages/rates/Rates.tsx
@@ -1,3 +1,19 @@
+import { tv } from "tailwind-variants";
+import { RatesCard } from "@/widgets/ratesCard";
+
+const ratesStyles = tv({
+  slots: {
+    wrapper: "min-h-full h-max-content flex justify-center py-5 lg:py-10",
+    ratesCard: "w-screen max-w-xs mx-3",
+  },
+});
+
 export default function Rates() {
-  return <></>;
+  const { wrapper, ratesCard } = ratesStyles();
+
+  return (
+    <article className={wrapper()}>
+      <RatesCard className={ratesCard()} />
+    </article>
+  );
 }
